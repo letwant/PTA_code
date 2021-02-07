50d200f0534926d72109ab597c8412ecd51ec613
letwant <letwant@163.com>
2021-02-07 22:10:34 +0800

    修改7-19支票问题的输出，得到ac

--- PTA_C++/7-19 支票面额.cpp
+++ PTA_C++/7-19 支票面额.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 #if 0
 int main(void) {
-	float y, f, n;
+	int y, f, n;						// 要定义成int，float后面会有小数点问题
 	cin >> n;
 	int count = 0;
 	for (y = 0; y < 100; y++) {
 		for (f = 0; f < 100; f++) {
-			if (f + y / 100 - n / 100 == 2 * y + 2 * f / 100) {
-				cout << y + f / 100;
+			if (98 * f - n == 199 * y) {
+				cout << y << "." << f;	// 这里一定要用“.”而不能用y*100+f，加会出错
 				break;
 			}
 			else {
--- PTA_C++/7-23 币值转换.cpp
+++ PTA_C++/7-23 币值转换.cpp
@@ -1,9 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 using namespace std;
-
+#if 0
 int main(void) {
 	int number;
 	cin >> number;
+	cout << "╪ссм";
 	return 0;
 }
+#endif
